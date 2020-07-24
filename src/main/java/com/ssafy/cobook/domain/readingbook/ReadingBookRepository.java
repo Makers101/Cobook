de24bb78a9c706272ba47cf4534405f70442d854
PandaHun <gnsgnsek@gmail.com>
2020-07-24 21:58:29 +0900

    [S03P12A111-2] feat/hun: refactor domain

--- src/main/java/com/ssafy/cobook/domain/readingbook/ReadingBookRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ssafy.cobook.domain.readingbook;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReadingBookRepository extends JpaRepository<ReadingBook, Long> {
-}
