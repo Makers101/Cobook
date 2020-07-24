9e3a83960e51d789095ed58bee75db2e52c0c0df
임효진 <qndnd1@naver.com>
2020-07-25 04:00:28 +0900

    [S03P12A111-10] feat/jin: Create Reset Password

--- src/main/java/com/ssafy/cobook/exception/ErrorCode.java
+++ src/main/java/com/ssafy/cobook/exception/ErrorCode.java
@@ -1,11 +1,11 @@
 package com.ssafy.cobook.exception;
 
 public enum ErrorCode {
-<<<<<<< src/main/java/com/ssafy/cobook/exception/ErrorCode.java
 
     UNEXPECTED(0, "서버 내부 에러."),
     UNEXPECTED_USER(1000, "존재하지 않는 회원입니다."),
     EXIST_USER_EMAIL(1100, "이미 가입된 이메일입니다."),
+    WRONG_EMAIL_CHECK_AUTH_CODE(1200, "인증코드가 틀립니다"),
     UNEXPECTED_CLUB(2000, "존재하지 않는 클럽입니다."),
     UNEXPECTED_POST(3000, "존재하지 않는 게시물입니다."),
     MEMBER_DUPLICATED_EMAIL(1100, "이미 가입된 이메일입니다."),
