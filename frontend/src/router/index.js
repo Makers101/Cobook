ec3503a28b9468f9082d5e566a7750231f056cd6
Soomin Hwang <smhwang0109@gmail.com>
2020-07-25 12:28:28 +0900

    [S03P12A111-117] feat/soom : Create PostList

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '@/views/accounts/Login.vue'
+import PostList from '@/views/posts/PostList.vue'
 
 Vue.use(VueRouter)
 
   const routes = [
   {
     path: '/',
-    name: 'Login',
-    component: Login
+    name: 'PostList',
+    component: PostList
   },
 ]
 
