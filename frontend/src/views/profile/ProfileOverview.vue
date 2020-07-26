85705b327e3b71c115777f3f6eb2c3d56fed5b5b
신채린 <chaelinshin96@gmail.com>
2020-07-26 17:31:50 +0900

    [S03P12A111-158] feat/lin : Update Profile Overview image size

--- frontend/src/views/profile/ProfileOverview.vue
+++ frontend/src/views/profile/ProfileOverview.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <img src="@/assets/ready.png" width="200px" class="mt-3">
+    <img src="@/assets/ready.png" width="150px" class="mt-3">
     <h3>현재 <strong>페이지 준비중</strong> 입니다.</h3>
     <p>이용에 불편을 드려 죄송합니다.<br>
     보다 나은 서비스 제공을 위하여 페이지 준비중에 있습니다.<br>
