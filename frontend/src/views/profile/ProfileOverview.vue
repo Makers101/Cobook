259615fd1cbfa82ad2469349d0110cba56ef9d68
신채린 <chaelinshin96@gmail.com>
2020-07-26 16:24:59 +0900

    [S03P12A111-158] feat/lin : Create Profile Overview

--- frontend/src/views/profile/ProfileOverview.vue
+++ frontend/src/views/profile/ProfileOverview.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <h3>Overview</h3>
+    <img src="@/assets/ready.png" width="200px" class="mt-3">
+    <h3>현재 <strong>페이지 준비중</strong> 입니다.</h3>
+    <p>이용에 불편을 드려 죄송합니다.<br>
+    보다 나은 서비스 제공을 위하여 페이지 준비중에 있습니다.<br>
+    빠른 시일 내에 더욱 나은 모습으로 찾아뵙겠습니다.</p>
   </div>
 </template>
 
