84e52349d403a8d9dee2f49628ebe4ab91706f9d
신채린 <chaelinshin96@gmail.com>
2020-07-28 14:06:17 +0900

    [S03P12A111-143] feat/lin : Update Profile axios

--- frontend/src/store/modules/profileStore.js
+++ frontend/src/store/modules/profileStore.js
@@ -4,13 +4,17 @@ import SERVER from '@/api/api'
 const profileStore = {
   namespaced: true,
   state: {
-    genres: null
+    genres: null,
+    profile: null,
   },
   getters: {
   },
   mutations: {
     SET_GENRES(state, genres) {
       state.genres = genres
+    },
+    SET_PROFILE(state, profile) {
+      state.profile = profile
     }
   },
   actions: {
@@ -18,6 +22,16 @@ const profileStore = {
       axios.get(SERVER.URL + SERVER.ROUTES.genres)
         .then(res => {
           commit('SET_GENRES', res.data)
+        })
+        .catch(err => {
+          console.log(err.response.data)
+        })
+    },
+    findProfile({ commit }, userId) {
+      axios.get(SERVER.URL + SERVER.ROUTES.profile + userId)
+        .then(res => {
+          console.log(res)
+          commit('SET_PROFILE', res.data)
         })
         .catch(err => {
           console.log(err.response.data)
