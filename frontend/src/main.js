8151ec609910329d9a4bb0fb5c1a62bdd38a0d3a
신채린 <chaelinshin96@gmail.com>
2020-07-29 16:24:54 +0900

    [S03P12A111-131] feat/lin : Create Vue Moment

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import VueMoment from 'vue-moment'
+Vue.use(VueMoment);
 
 Vue.config.productionTip = false
 
