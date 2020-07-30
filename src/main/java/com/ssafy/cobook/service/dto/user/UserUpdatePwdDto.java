373e19a004b7ec406c6271d57e8a9e65a16f2e2f
임효진 <qndnd1@naver.com>
2020-07-31 05:11:19 +0900

    [S03P12A111-218] fix/jin: Update DTO for Password

--- src/main/java/com/ssafy/cobook/service/dto/user/UserUpdatePwdDto.java
+++ src/main/java/com/ssafy/cobook/service/dto/user/UserUpdatePwdDto.java
@@ -8,4 +8,5 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserUpdatePwdDto {
     private String password;
+    private String passwordConfirm;
 }
