3c420b2371bd47b4d6e78fe72d6c695762823ee1
PandaHun <gnsgnsek@gmail.com>
2020-07-31 03:16:21 +0900

    [S03P12A111-49] hun: Refactor Notification API

--- src/main/java/com/ssafy/cobook/domain/notification/NotificationRepository.java
+++ src/main/java/com/ssafy/cobook/domain/notification/NotificationRepository.java
@@ -3,8 +3,10 @@ package com.ssafy.cobook.domain.notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
     List<Notification> findAllByToId(Long to);
+    Optional<Notification> findByFromIdAndToIdAndDataIdAndTypes(Long fromId, Long toId, Long dataId, String types);
 }
