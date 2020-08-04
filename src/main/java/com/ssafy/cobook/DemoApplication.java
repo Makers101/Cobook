ece56dfa7bd59a53ab0119871f95637031b943fd
gnsgnsek <gnsgnsek@gmail.com>
2020-08-04 18:01:02 +0900

    [S03P13A111-6] feat/hun: Create SSL Setting

--- src/main/java/com/ssafy/cobook/DemoApplication.java
+++ src/main/java/com/ssafy/cobook/DemoApplication.java
@@ -1,7 +1,10 @@
 package com.ssafy.cobook;
 
+import org.apache.catalina.connector.Connector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -36,4 +39,16 @@ public class DemoApplication {
 		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 	}
 
+	@Bean
+	public ServletWebServerFactory serverFactory() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		tomcat.addAdditionalTomcatConnectors(createStandardConnector()); // 톰캣에 Connector 추가
+		return tomcat;
+	}
+
+	private Connector createStandardConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setPort(8080); // 포트 설정(https랑 달라야함)
+		return connector;
+	}
 }
