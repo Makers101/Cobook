0076fd6808517c0c15d963da01b3a141f23abb0d
박선환 <witty_man@naver.com>
2020-08-05 15:12:48 +0900

    [S03P12A111-101] feat/sun: Update ClubUpdate residence

--- frontend/src/views/clubs/ClubUpdate.vue
+++ frontend/src/views/clubs/ClubUpdate.vue
@@ -27,15 +27,13 @@
           ></v-progress-linear>
         </template>
 
-        <v-form
-          v-model="valid"
-          :lazy-validation="lazy"
-        >
+        <!-- club-update-form -->
+        <v-form v-model="valid" :lazy-validation="lazy">
           <v-container>
             <v-row>
-              <v-col
-                cols="12"
-              >
+
+              <!-- club-update-name -->
+              <v-col cols="12">
                 <v-text-field
                   v-model="clubUpdateData.basicData.name"
                   color="blue-grey lighten-2"
@@ -46,7 +44,14 @@
                 ></v-text-field>
               </v-col>
               
+              <!-- club-update-clubImg -->
               <v-col cols="12">
+                <div v-if="selectedClub.clubImg && !clubImg">
+                  <img class="img-fluid club-image rounded" :src="selectedClub.clubImg" :alt="selectedClub.name">
+                </div>
+                <div v-if="previewImg && clubImg">
+                  <img class="img-fluid club-image rounded" :src="previewImg" :alt="selectedClub.name">
+                </div>
                 <v-file-input
                   v-model="clubImg"
                   enctype="multipart/form-data"
@@ -55,15 +60,14 @@
                   show-size
                   prepend-icon=""
                   chips
-                  :rules="[v => !!v || '필수항목입니다.', v => !v || v.size < 2000000 || '이미지 크기는 최대 2MB 입니다.' ]"
+                  @change="previewImage"
+                  :rules="[v => !v || v.size < 2000000 || '이미지 크기는 최대 2MB 입니다.' ]"
                 >
                 </v-file-input>
               </v-col>
 
-              <v-col
-                cols="6"
-                md="6"
-              >
+              <!-- club-update-leader -->
+              <v-col cols="6">
                 <v-text-field
                   v-if="myaccount"
                   v-model="myaccount.nickName"
@@ -72,20 +76,10 @@
                   :rules="[v => !!v || '필수항목입니다.']"
                   label="클럽장"
                 ></v-text-field>
-                <v-text-field
-                  v-else
-                  v-model="myaccount.nickName"
-                  disabled
-                  color="blue-grey lighten-2"
-                  :rules="[v => !!v || '필수항목입니다.']"
-                  label="클럽장"
-                ></v-text-field>
               </v-col>
 
-              <v-col
-                cols="12"
-                md="12"
-              >
+              <!-- club-update-onelineDescription -->
+              <v-col cols="12">
                 <v-text-field
                   v-model="clubUpdateData.basicData.onelineDescription"
                   color="blue-grey lighten-2"
@@ -95,10 +89,9 @@
                   label="한 줄 설명"
                 ></v-text-field>
               </v-col>
-              <v-col
-                cols="12"
-                md="12"
-              >
+
+              <!-- club-update-description -->
+              <v-col cols="12">
                 <v-textarea
                   v-model="clubUpdateData.basicData.description"
                   color="blue-grey lighten-2"
@@ -109,6 +102,7 @@
                 ></v-textarea>
               </v-col>
 
+              <!-- club-update-genres -->
               <v-col cols="12">
                 <v-autocomplete
                   v-model="clubUpdateData.basicData.genres"
@@ -154,22 +148,37 @@
                 </v-autocomplete>
               </v-col>
 
-              <v-col
-                cols="12"
-                md="12"
-              >
-                <v-text-field
+              <!-- club-update-residence -->
+              <v-col class="mt-5" cols="12">
+                <div class="d-flex justify-content-start">
+                  <label class="v-label theme--light">주 모임 지역</label>
+                </div>
+                <v-radio-group
                   v-model="clubUpdateData.basicData.residence"
-                  color="blue-grey lighten-2"
-                  counter
-                  maxlength="10"
                   :rules="[v => !!v || '필수항목입니다.']"
-                  label="주 모임지역"
-                ></v-text-field>
+                >
+                  <div class="d-flex justify-content-start align-items-center">
+                    <v-radio label="온라인" value="온라인"></v-radio>
+                  </div>
+                  <div class="d-flex justify-content-start align-items-center">
+                    <v-radio label="오프라인" :value="offlineResidence"></v-radio>
+                    <v-text-field
+                      class="mx-3 mb-2"
+                      v-model="offlineResidence"
+                      counter
+                      maxlength="10"
+                      :disabled="!offlineEnabled"
+                      label="오프라인 모임 장소"
+                      :rules="residenceRules"
+                    ></v-text-field>
+                  </div>
+                </v-radio-group>
               </v-col>
+
             </v-row>
           </v-container>
 
+          <!-- club-update-button -->
           <v-card-actions class="d-flex justify-content-end">
             <v-btn
               :disabled="!valid"
@@ -179,6 +188,7 @@
               클럽 수정
             </v-btn>
           </v-card-actions>
+
         </v-form>
       </v-card>
     </v-app>
@@ -200,17 +210,40 @@ export default {
           genres: [],
         },
         clubId: this.$route.params.clubId,
-        clubImgFormData: new FormData()
+        clubImgFormData: null
       },
       clubImg: null,
+      previewImg: null,
       valid: true,
       lazy:false,
       searchGenre: null,
+      offlineResidence: null
     }
   },
   computed: {
-    ...mapState(['genres', 'myaccount', 'users']),
-    ...mapState('clubStore', ['selectedClub'])
+    ...mapState(['genres', 'myaccount']),
+    ...mapState('clubStore', ['selectedClub']),
+    offlineEnabled() {
+      if (this.clubUpdateData.basicData.residence === null && this.offlineResidence === null) {
+        return false
+      } else if (this.clubUpdateData.basicData.residence === '온라인' && this.offlineResidence !== '온라인') {
+        return false
+      } else {
+        return true
+      }
+    },
+    residenceRules() {
+      const tempRules = []
+      if (this.offlineEnabled) {
+        const rule = v => !!v || '필수항목입니다.'
+        tempRules.push(rule)
+      }
+      if (this.offlineResidence) {
+        const rule = v => v !== '온라인' || '오프라인 장소를 작성해주세요 :)'
+        tempRules.push(rule)
+      }
+      return tempRules
+    }
   },
   methods: {
     ...mapActions('clubStore', ['findClub', 'updateClub']),
@@ -226,10 +259,25 @@ export default {
     validate() {
       this.$refs.form.validate()
     },
-    // clickUpdate() {
-    //   this.clubCreateData.clubImgFormData.append('clubImg', this.clubImg)
-    //   this.createClub(this.clubCreateData)
-    // }
+    clickUpdate() {
+      if (this.clubImg) {
+        this.clubUpdateData.clubImgFormData = new FormData()
+        this.clubUpdateData.clubImgFormData.append('clubImg', this.clubImg)
+      }
+      if (this.offlineEnabled) {
+        this.clubUpdateData.basicData.residence = this.offlineResidence
+      }
+      this.updateClub(this.clubUpdateData)
+    },
+    previewImage() {
+      if (this.clubImg) {
+        let reader = new FileReader();
+        reader.onload = e => {
+          this.previewImg = e.target.result
+        };
+        reader.readAsDataURL(this.clubImg);
+      }
+    }
   },
   created() {
     this.findClub(this.$route.params.clubId)
@@ -238,6 +286,9 @@ export default {
     this.clubUpdateData.basicData.name = this.selectedClub.name
     this.clubUpdateData.basicData.onelineDescription = this.selectedClub.onelineDescription
     this.clubUpdateData.basicData.description = this.selectedClub.description
+    if (this.selectedClub.residence !== '온라인') {
+      this.offlineResidence = this.selectedClub.residence
+    }
     this.clubUpdateData.basicData.residence = this.selectedClub.residence
     this.selectedClub.genres.forEach(genre => {
       this.clubUpdateData.basicData.genres.push(genre.id)
@@ -266,5 +317,9 @@ export default {
     top: 50%;
     left: 50%;
     transform: translate( -50%, -50% );
+  }
+
+  .club-image {
+    max-height: 200px; 
   }
 </style>
