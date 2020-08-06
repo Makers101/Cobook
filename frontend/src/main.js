d93f70a2c605ee7a047f448013b193c8cfaf6aed
황수민 <0109hsm@naver.com>
2020-08-06 09:33:37 +0900

    [S03P12A111-4] Delete main.js firebase setting

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,9 +6,6 @@ import vuetify from './plugins/vuetify';
 import VueMoment from 'vue-moment'
 import VueCookies from 'vue-cookies'
 
-import firebase from 'firebase/app'
-import 'firebase/messaging'
-
 import GAuth from 'vue-google-oauth2'
 
 const gauthOption = {
@@ -17,43 +14,6 @@ const gauthOption = {
     prompt: 'consent',
     fetch_basic_profile: true
 }
-
-const firebaseConfig = {
-    apiKey: "AIzaSyA9KKQn0uKuErmQMJsMbhw25-iG8chHLdI",
-    authDomain: "co-book-original.firebaseapp.com",
-    databaseURL: "https://co-book-original.firebaseio.com",
-    projectId: "co-book-original",
-    storageBucket: "co-book-original.appspot.com",
-    messagingSenderId: "21513194733",
-    appId: "1:21513194733:web:5ac45b8faee796d5b910e4",
-    measurementId: "G-YNYKTYY7B8"
-};
-
-firebase.initializeApp(firebaseConfig);
-
-const messaging = firebase.messaging()
-
-messaging.usePublicVapidKey('BMxEbAZ8_VAS1w4fZ06Kd-TL8eWPdxlFR4aRmQj1NHpEdAyVwKShOg8YLF_lJbhdwMpuS09jbMbbi5PDGYn_UoA')
-
-// 알림 수신을 위한 사용자 권한 요청
-Notification.requestPermission()
-    .then((permission) => {
-        console.log('permission ', permission)
-        if (permission !== 'granted') {
-            alert('알림을 허용해주세요')
-        }
-    })
-
-// TODO: Send token to server for send notification
-messaging.getToken()
-    .then(console.log)
-
-// Handle received push notification at foreground
-messaging.onMessage(payload => {
-    console.log(payload)
-    alert(payload.data.message)
-})
-
 
 Vue.use(VueCookies)
 Vue.use(VueMoment);
