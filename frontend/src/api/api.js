bbd09dc454160e62ed92dffec4e520bc59f7d804
신채린 <chaelinshin96@gmail.com>
2020-08-06 10:41:08 +0900

    [S03P13A111-9] feat/lin : Update api url for social login

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -17,7 +17,7 @@ export default {
     // accounts
     myaccount: '/users',
     signup: '/users/signup',
-    social: 'users/social',
+    social: '/users/social',
     login: '/users/signin',
     password: '/users/password',
     changepassword: '/users/resetPassword/update',
