862a30fca6514e7209ca9391eb8839ae4fed92a1
신채린 <chaelinshin96@gmail.com>
2020-08-07 11:07:22 +0900

    [S03P13A111-9] feat/lin : Update Login notification

--- frontend/src/views/accounts/SignupKakao.vue
+++ frontend/src/views/accounts/SignupKakao.vue
@@ -32,6 +32,7 @@
 
 <script>
 import SERVER from '@/api/api'
+import Swal from 'sweetalert2'
 import axios from 'axios'
 import router from '@/router'
 import cookies from 'vue-cookies'
@@ -84,6 +85,21 @@ export default {
       axios.post(SERVER.URL + SERVER.ROUTES.social, info)
         .then(res => {
           cookies.set("auth-token", res.data);
+          const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 2000,
+            timerProgressBar: true,
+            onOpen: (toast) => {
+              toast.addEventListener('mouseenter', Swal.stopTimer)
+              toast.addEventListener('mouseleave', Swal.resumeTimer)
+              }
+           })
+           Toast.fire({
+            icon: 'success',
+            title: "로그인에 성공하였습니다."
+          })
           router.push('/')
         })
         .catch (err => {
