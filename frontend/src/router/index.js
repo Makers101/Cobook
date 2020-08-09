ceaa0ad01404c7c1055e1f06045a4d173941bf0d
신채린 <chaelinshin96@gmail.com>
2020-08-10 02:45:31 +0900

    [S03P12A111-4] feat/lin : Create bookStore for book detail

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -38,6 +38,9 @@ import OnedayEventDetail from '@/views/onedayevents/OnedayEventDetail'
 import OnedayEventCreate from '@/views/onedayevents/OnedayEventCreate'
 import OnedayEventUpdate from '@/views/onedayevents/OnedayEventUpdate'
 
+// books
+import Book from '@/views/books/Book'
+
 //pagenotfound
 import PageNotFound from '@/views/PageNotFound'
 
@@ -210,6 +213,12 @@ Vue.use(VueRouter)
     path: '/onedayevents/:onedayEventId/update',
     name: 'OnedayEventUpdate',
     component: OnedayEventUpdate
+  },
+  // Books
+  {
+    path: '/books/:bookId',
+    name: 'Book',
+    component: Book
   },
   // 404 Pages
   {
