c5e9b5873e66d6cba8cafac92bba7b05140e7b99
신채린 <chaelinshin96@gmail.com>
2020-08-12 14:05:13 +0900

    [S03P13A111-11] feat/lin : Update search icons

--- frontend/src/views/search/Search.vue
+++ frontend/src/views/search/Search.vue
@@ -1,6 +1,46 @@
 <template>
-  <div class="custom-container">
-    
+  <div>
+    <div class="col-3 text-left ml-2">
+      <h4>검색 결과:</h4>
+      <p>000</p>
+      <hr>
+      <h5>필터</h5>
+      <div>
+        <div class="float-left bg-beige mr-2" style="border-radius:50%">
+          <i class="fas fa-user p-2"></i>
+        </div>
+        <p >유저</p>
+      </div>
+      <div>
+        <div class="float-left bg-beige mr-2" style="border-radius:50%">
+          <i class="fas fa-comment-dots p-2"></i>
+        </div>
+        <p>리뷰</p>
+      </div>
+      <div>
+        <div class="float-left bg-beige mr-2" style="border-radius:50%">
+          <i class="fas fa-book-open p-2"></i>
+        </div>
+        <p>도서</p>
+      </div>
+      <div>
+        <div class="float-left bg-beige mr-2" style="border-radius:50%">
+           <i class="fas fa-users p-2"></i>
+        </div>
+        <p>클럽</p>
+      </div>
+      <div>
+        <div class="float-left bg-beige mr-2" style="border-radius:50%;">
+          <span class="mdi mdi-calendar-star p-2" width="50px"></span>
+        </div>
+        <p>원데이 이벤트</p>
+        
+      </div>
+      
+    </div>
+    <div class="col-9">
+
+    </div>
   </div>
 </template>
 
