7c6cf99d40066cae91ad9d45c29306d62fb0d0a5
신채린 <chaelinshin96@gmail.com>
2020-08-12 19:48:01 +0900

    [S03P12A111-58] feat/lin : Update SearchBook design structure

--- frontend/src/views/search/SearchBook.vue
+++ frontend/src/views/search/SearchBook.vue
@@ -7,18 +7,33 @@
             <div class="card h-100">
               <img class="bookImg card-img-top" src="https://images-na.ssl-images-amazon.com/images/I/712KSYstH9L.jpg">
               <div class="overlay">
-                <div class="text">Hello World</div>
+                <div class="text">
+                  <p>Little Chinese Seamstress</p>
+                  <small>By. Dal Sijie</small>
+                </div>
               </div>
             </div>
           </div>
           <div class="col mb-4 p-3">
             <div class="card h-100">
               <img class="bookImg card-img-top" src="https://images-na.ssl-images-amazon.com/images/I/712KSYstH9L.jpg">
+              <div class="overlay">
+                <div class="text">
+                  <p>Little Chinese Seamstress</p>
+                  <small>By. Dal Sijie</small>
+                </div>
+              </div>
             </div>
           </div>
           <div class="col mb-4 p-3">
             <div class="card h-100">
               <img class="bookImg card-img-top" src="https://images-na.ssl-images-amazon.com/images/I/712KSYstH9L.jpg">
+              <div class="overlay">
+                <div class="text">
+                  <p>Little Chinese Seamstress</p>
+                  <small>By. Dal Sijie</small>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -36,5 +51,62 @@ export default {
 <style scoped>
 .card {
   box-shadow: 8px 8px 10px 3px rgb(0, 0, 0, 0.2)
+}
+
+.image {
+  display: block;
+  width: 100%;
+  height: auto;
+}
+
+.card .overlay {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0;
+  /* transition: .5s ease; */
+  background-color: rgba(0, 0, 0, 0.7);
+}
+
+.card:hover .overlay {
+  opacity: 1;
+  -webkit-transition: all 0.4s ease-in-out 0s;
+  -moz-transition: all 0.4s ease-in-out 0s;
+  transition: all 0.4s ease-in-out 0s;
+}
+.card:hover .text {
+  top: 50%;
+  left: 50%;
+  opacity: 1;
+}
+
+.text {
+  color: white;
+  font-size: 20px;
+  position: absolute;
+  padding-left: 0.3em;
+  padding-right: 0.3em;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  transition: all 0.3s ease-in-out 0s;
+  text-align: center;
+}
+
+.text p {
+  color: #fff;
+  padding-left: 5px;
+  font-size: 0.75em;
+  font-weight: 300;
+  letter-spacing: 0.15em;
+  margin-bottom: 0.5em;
+  white-space: pre-wrap;
+  word-break: keep-all;
 }
 </style>
