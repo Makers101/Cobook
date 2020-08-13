a8d7e49a5910250c088239383548f2f75f73a9ab
신채린 <chaelinshin96@gmail.com>
2020-08-13 11:18:31 +0900

    [S03P12A111-60] feat/lin : Update SearchUser v-else message

--- frontend/src/views/search/SearchUser.vue
+++ frontend/src/views/search/SearchUser.vue
@@ -1,31 +1,37 @@
 <template>
   <div class="my-3">
-    <div v-for="user in users" :key="user.id" class="d-flex justify-content-center w-100 mt-3">
-      <div class="d-flex justify-content-between user">
-        <div class="d-flex">
-          <!-- 프로필 이미지 -->
-          <img
-            v-if="!user.profileImg"
-            class="img-fluid feed-profile-img ml-3 mr-3"
-            style="max-width: 50%; border-radius: 50%;" 
-            src="http://bit.do/anonymouseuser" 
-            alt="유저 프로필 사진">
-          <img 
-            v-else
-            class="img-fluid feed-profile-img ml-3 mr-3"
-            style="max-width: 50%; border-radius: 50%;" 
-            :src="user.profileImg" alt="작성자 프로필 사진">
-          <!-- 프로필 닉네임 -->
-          <p class="user-nickName pointer" @click="selectUser(user.id)"><strong>{{ user.nickName }}</strong></p>
-        </div>
-        <!-- 팔로잉 여부 -->
-        <div class="d-flex align-items-center mr-3">
-          <button v-if="user.isFollow" class="btn btn-following" @click="clickedFollow(user, 'unfollow')">언팔로잉</button>
-          <button v-else class="btn btn-follow" @click="clickedFollow(user, 'follow')">팔로우</button>
-          <!-- <button v-if="checkFollow(profile)" class="btn px-4 btn-following" @click="clickedFollow(profile, 'unfollow')">언팔로우</button>
-          <button v-else class="btn btn-follow px-4" @click="clickedFollow(profile, 'follow')">팔로우</button> -->
+    <div v-if="users.length">
+      <div v-for="user in users" :key="user.id" class="d-flex justify-content-center w-100 mt-3">
+        <div class="d-flex justify-content-between user">
+          <div class="d-flex profile-image">
+            <!-- 프로필 이미지 -->
+            <img
+              v-if="!user.profileImg"
+              class="img-fluid feed-profile-img ml-3 mr-3"
+              style="max-width: 50%; border-radius: 50%;" 
+              src="http://bit.do/anonymouseuser" 
+              alt="유저 프로필 사진">
+            <img 
+              v-else
+              class="img-fluid feed-profile-img ml-3 mr-3"
+              style="max-width: 50%; border-radius: 50%;" 
+              :src="user.profileImg" alt="작성자 프로필 사진">
+            <!-- 프로필 닉네임 -->
+            <p class="user-nickName pointer" @click="selectUser(user.id)"><strong>{{ user.nickName }}</strong></p>
+          </div>
+          <!-- 팔로잉 여부 -->
+          <div class="d-flex align-items-center mr-3">
+            <button v-if="user.isFollow" class="btn btn-following" @click="clickedFollow(user, 'unfollow')">언팔로잉</button>
+            <button v-else class="btn btn-follow" @click="clickedFollow(user, 'follow')">팔로우</button>
+            <!-- <button v-if="checkFollow(profile)" class="btn px-4 btn-following" @click="clickedFollow(profile, 'unfollow')">언팔로우</button>
+            <button v-else class="btn btn-follow px-4" @click="clickedFollow(profile, 'follow')">팔로우</button> -->
+          </div>
         </div>
       </div>
+    </div>
+    <div class="no-result" v-else>
+      <img src="https://user-images.githubusercontent.com/57381062/88909174-c11bb500-d295-11ea-81b6-90c7bc3642ab.png" width="150px" class="mt-3">
+      <h3 class="mt-3">검색된 키워드의 유저가 없습니다. </h3>
     </div>
   </div>
 </template>
@@ -79,7 +85,7 @@ p {
   box-shadow: 5px 5px 10px 1px rgb(0, 0, 0, 0.2);
 }
 
-img {
+.profile-image img {
   border: 1px solid rgb(0, 0, 0, 0.2);
   border-radius: 50%;
   width: 60px;
@@ -99,5 +105,10 @@ img {
   color: #88A498 !important;
 }
 
-
+.no-result {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin: -150px 0px 0px -200px;
+}
 </style>
