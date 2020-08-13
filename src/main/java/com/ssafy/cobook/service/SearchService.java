e90dc38f6e905c278b7d912f8f8ae004a66ab704
임효진 <qndnd1@naver.com>
2020-08-13 20:32:28 +0900

    [S03P12A111-41] feat/jin: Update Searching Post

--- src/main/java/com/ssafy/cobook/service/SearchService.java
+++ src/main/java/com/ssafy/cobook/service/SearchService.java
@@ -13,6 +13,7 @@ import com.ssafy.cobook.service.dto.book.BookBySearchResDto;
 import com.ssafy.cobook.service.dto.club.ClubBySearchResDto;
 import com.ssafy.cobook.service.dto.onedayevent.OneDayEventBySearchDto;
 import com.ssafy.cobook.service.dto.post.PostBySearchResDto;
+import com.ssafy.cobook.service.dto.tag.TagByPostDto;
 import com.ssafy.cobook.service.dto.user.UserBySearchResDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,6 +22,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -60,7 +62,25 @@ public class SearchService {
     }
 
     public List<PostBySearchResDto> searchPosts(String keyword) {
-        return postRepository.findByKeyword(keyword);
+//        List<PostBySearchResDto> postList = (postRepository.findByExceptKeyword(keyword).stream()
+//                .filter(post -> checkTags(post, keyword))
+//                .collect(Collectors.toList()));
+
+        return postRepository.findByKeyword(keyword).stream()
+                .collect(Collectors.toList());
+
+//        return postList;
+    }
+
+    private boolean checkTags(PostBySearchResDto post, String keyword) {
+        List<TagByPostDto> tagList = post.getTags();
+
+        for (TagByPostDto tag : tagList){
+            if(tag.getName().equals(keyword)){
+                return true;
+            }
+        }
+        return false;
     }
 
     public List<ClubBySearchResDto> searchClubs(String keyword) {
