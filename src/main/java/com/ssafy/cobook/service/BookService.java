d612dc852facd00b7f94a39db3ee035a5126450c
PandaHun <gnsgnsek@gmail.com>
2020-08-14 09:16:33 +0900

    [S03P12A111-36] feat/hun: Create Posts API

--- src/main/java/com/ssafy/cobook/service/BookService.java
+++ src/main/java/com/ssafy/cobook/service/BookService.java
@@ -2,10 +2,13 @@ package com.ssafy.cobook.service;
 
 import com.ssafy.cobook.domain.book.Book;
 import com.ssafy.cobook.domain.book.BookRepository;
+import com.ssafy.cobook.domain.genre.Genre;
+import com.ssafy.cobook.domain.genre.GenreRepository;
 import com.ssafy.cobook.domain.post.PostRepository;
 import com.ssafy.cobook.exception.BaseException;
 import com.ssafy.cobook.exception.ErrorCode;
 import com.ssafy.cobook.service.dto.book.BookDetailResponseDto;
+import com.ssafy.cobook.service.dto.book.BookResponseByGenres;
 import com.ssafy.cobook.service.dto.book.BookResponseDto;
 import com.ssafy.cobook.service.dto.post.PostByMembersResDto;
 import lombok.RequiredArgsConstructor;
@@ -24,6 +27,7 @@ public class BookService {
 
     private final BookRepository bookRepository;
     private final PostRepository postRepository;
+    private final GenreRepository genreRepository;
 
     public List<BookResponseDto> getAllBooks() {
         return bookRepository.findAll().stream()
@@ -33,10 +37,19 @@ public class BookService {
 
     public BookDetailResponseDto getDetials(Long bookId) {
         Book book = bookRepository.findById(bookId)
-                .orElseThrow(()->new BaseException(ErrorCode.UNEXPECTED_BOOK));
+                .orElseThrow(() -> new BaseException(ErrorCode.UNEXPECTED_BOOK));
         List<PostByMembersResDto> posts = postRepository.findAllByBook(book).stream()
                 .map(PostByMembersResDto::new)
                 .collect(Collectors.toList());
         return new BookDetailResponseDto(book, posts);
+    }
+
+    public List<BookResponseByGenres> findBookByGenre(Long genreId) {
+        Genre genre = genreRepository.findById(genreId)
+                .orElseThrow(() -> new BaseException(ErrorCode.INVALID_GENRE));
+        return bookRepository.findAllByGenre(genre).stream()
+                .limit(5)
+                .map(b -> new BookResponseByGenres(b, genre))
+                .collect(Collectors.toList());
     }
 }
