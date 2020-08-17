cb82954d42eb1ea5ddd29d9fb18f83e12ed33be3
임효진 <qndnd1@naver.com>
2020-08-17 15:49:48 +0900

    [S03P12A111-229] fix/jin: Fix Getting follower and following

--- src/main/java/com/ssafy/cobook/service/dto/user/UserByFollowDto.java
+++ src/main/java/com/ssafy/cobook/service/dto/user/UserByFollowDto.java
@@ -7,16 +7,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Getter
 public class UserByFollowDto {
-    Long toUserId;
+    Long id;
     String nickname;
     String profileImg;
     Boolean isFollow; // 내가 toUser를 팔로우 했는지 안했는지 여부라고 보면됨!
 
     public UserByFollowDto(User user) {
-        this.toUserId = user.getId();
+        this.id = user.getId();
         this.nickname = user.getNickName();
         if (user.getProfileImg() != null) {
-            this.profileImg = "https://i3a111.p.ssafy.io:8090/api/profile/images/" + this.toUserId;
+            this.profileImg = "https://i3a111.p.ssafy.io:8090/api/profile/images/" + this.id;
         }
         this.isFollow = false;
     }
