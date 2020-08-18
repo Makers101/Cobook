82b668ee507c6ec7d81e48f28468289f54aa955d
임효진 <qndnd1@naver.com>
2020-08-18 13:32:01 +0900

    [S03P12A111-229] fix/jin: Fix OAuth2 Validation

--- src/main/java/com/ssafy/cobook/service/dto/user/OAuth2LoginDto.java
+++ src/main/java/com/ssafy/cobook/service/dto/user/OAuth2LoginDto.java
@@ -14,16 +14,10 @@ import java.util.Collections;
 @NoArgsConstructor
 public class OAuth2LoginDto {
 
-    @NotEmpty(message = "닉네임을 입력해주세요.")
-    @Length(min = 2, max = 128)
     private String nickname;
 
-    @NotEmpty(message = "이메일을 입력해주세요.")
-    @Email(message = "이메일 형식이 올바르지 않습니다.")
     private String email;
 
-    @NotEmpty
-    @Length(max = 128)
     private String platformType;
 
     public User toEntity(String platformType) {
